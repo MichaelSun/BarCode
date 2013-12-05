071d7ac8f196aea651d345956e8ef840a07c4b75
Robert Sun <sunting.bcwl@gmail.com>
2013-12-05 16:30:53 +0800

    使用整型绘图，不用浮点数，减少误差

--- src/com/qrcode/sdk/QRBorder.java
+++ src/com/qrcode/sdk/QRBorder.java
@@ -1,17 +1,33 @@
 package com.qrcode.sdk;
 
 import android.graphics.Path;
-import android.graphics.RectF;
+import android.graphics.Rect;
 
 public abstract class QRBorder {
 
-	protected int mWidth = 0;
+	protected int mWidth;
 
-	protected int mHeight = 0;
+	protected int mHeight;
+
+	protected int mLeftPadding;
+
+	protected int mTopPadding;
+
+	protected Rect mInsideRect;
+
+	protected int mBoxSize;
 
 	public QRBorder(int width, int height) {
 		mWidth = width;
 		mHeight = height;
+	}
+
+	public int getLeftPadding() {
+		return mLeftPadding;
+	}
+
+	public int getTopPadding() {
+		return mTopPadding;
 	}
 
 	public int getWidth() {
@@ -22,7 +38,13 @@ public abstract class QRBorder {
 		return mHeight;
 	}
 
-	protected abstract RectF getInsideArea();
+	public Rect getInsideArea() {
+		return mInsideRect;
+	}
+	
+	public int getBoxSize() {
+		return mBoxSize;
+	}
 
 	protected abstract Path getClipPath();
 
--- src/com/qrcode/sdk/QRRhombusBorder.java
+++ src/com/qrcode/sdk/QRRhombusBorder.java
@@ -1,35 +1,34 @@
 package com.qrcode.sdk;
 
 import android.graphics.Path;
-import android.graphics.RectF;
+import android.graphics.Rect;
 
 public class QRRhombusBorder extends QRBorder {
 
-	private RectF mInsideRect;
-
-	private float mInsideW = 0;
-
-	public QRRhombusBorder(int width, int height) {
+	public QRRhombusBorder(int width, int height, int qrDimension) {
 		super(width, height);
-		mInsideW = (width < height ? width : height) / 2.0f;
-		mInsideRect = new RectF((mWidth - mInsideW) / 2,
-				(mHeight - mInsideW) / 2, (mWidth + mInsideW) / 2,
-				(mHeight + mInsideW) / 2);
-	}
 
-	@Override
-	protected RectF getInsideArea() {
-		return mInsideRect;
+		int insideWidth = Math.min(width, height) >> 1;
+		mBoxSize = insideWidth / qrDimension;
+		int padding = (insideWidth % qrDimension) >> 1;
+
+		mLeftPadding = (width >> 1) - insideWidth + (padding << 1);
+		mTopPadding = (height >> 1) - insideWidth + (padding << 1);
+
+		mInsideRect = new Rect((width >> 1) - (insideWidth >> 1) + padding,
+				(height >> 1) - (insideWidth >> 1) + padding, (width >> 1)
+						+ (insideWidth >> 1) - padding, (height >> 1)
+						+ (insideWidth >> 1) - padding);
 	}
 
 	@Override
 	protected Path getClipPath() {
 		Path path = new Path();
-		path.moveTo(0, mHeight / 2.0f);
-		path.lineTo(mWidth / 2.0f, 0);
-		path.lineTo(mWidth, mHeight / 2.0f);
-		path.lineTo(mWidth / 2.0f, mHeight);
-		path.lineTo(0, mHeight / 2.0f);
+		path.moveTo(mLeftPadding, mHeight >> 1);
+		path.lineTo(mWidth >> 1, mTopPadding);
+		path.lineTo(mWidth - mLeftPadding, mHeight >> 1);
+		path.lineTo(mWidth >> 1, mHeight - mTopPadding);
+		path.close();
 		return path;
 	}
 
